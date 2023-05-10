130c9d4fa12d55d4ad9557956fa9a7dd30da14ca
Razvan <2razvan5@gmail.com>
2023-05-10 20:58:56 +0300

    js corect

--- Frontend/pages/games/lol/lol.js
+++ Frontend/pages/games/lol/lol.js
@@ -2,17 +2,25 @@ var audio = document.getElementById("myAudio");
 var gifContainer = document.querySelector(".gif-container");
 var gif = gifContainer.querySelector("img");
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.key === "f") {
     audio.play();
     gifContainer.style.display = "block";
     gif.classList.add("animate");
 
-    setTimeout(function() {
+    setTimeout(function () {
       audio.pause();
       audio.currentTime = 0;
       gif.classList.remove("animate");
       gifContainer.style.display = "none";
     }, 5000);
+  }
+});
+
+const sound = document.getElementById('ping');
+
+document.addEventListener('click', (event) => {
+  if (event.which === 1) {
+    sound.play();
   }
 });
