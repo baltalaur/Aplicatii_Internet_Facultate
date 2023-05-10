a54b5ecd3badcf46185719bce1176b58af49ecaa
Razvan <2razvan5@gmail.com>
2023-05-10 20:53:38 +0300

    js but nok

---
+++ Frontend/pages/games/lol/lol.js
@@ -0,0 +1,18 @@
+var audio = document.getElementById("myAudio");
+var gifContainer = document.querySelector(".gif-container");
+var gif = gifContainer.querySelector("img");
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "f") {
+    audio.play();
+    gifContainer.style.display = "block";
+    gif.classList.add("animate");
+
+    setTimeout(function() {
+      audio.pause();
+      audio.currentTime = 0;
+      gif.classList.remove("animate");
+      gifContainer.style.display = "none";
+    }, 5000);
+  }
+});
